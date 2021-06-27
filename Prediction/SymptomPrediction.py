50316cf70d2f9fa7f5be5c65745542ede2495bb8
Bilal Akbar <muhammadbilalakbar021@gmail.com>
2021-06-27 11:08:41 -0700

    symp

--- Prediction/SymptomPrediction.py
+++ Prediction/SymptomPrediction.py
@@ -6,26 +6,32 @@ load_model = pickle.load(open(file, 'rb'))
 
 
 def SymptomPrediction(itching, skin_rash, nodal_skin_eruptions, continuous_sneezing, shivering, chills, joint_pain,
-            muscle_wasting, vomiting, fatigue, weight_loss, restlessness, irregular_sugar_level, cough, high_fever,
-            breathlessness, sweating, headache):
+            muscle_wasting, vomiting, fatigue, weight_loss, restlessness, lethargy, patches_in_throat, irregular_sugar_level, cough, high_fever,
+            breathlessness, sweating, headache, yellowish_skin, dark_urine, nausea, loss_of_appetite, pain_behind_the_eyes,
+            back_pain , abdominal_pain, diarrhoea , mild_fever, yellow_urine, yellowing_of_eyes, acute_liver_failure,
+            swelled_lymph_nodes, malaise, blurred_and_distorted_vision, phlegm, throat_irritation, redness_of_eyes,
+            sinus_pressure, runny_nose, congestion, chest_pain, fast_heart_rate, obesity, excessive_hunger, extra_marital_contacts,
+            loss_of_smell, muscle_pain, red_spots_over_body, dischromic_patches, watering_from_eyes, increased_appetite, polyuria,
+            rusty_sputum, receiving_blood_transfusion, receiving_unsterile_injections, stomach_bleeding):
 
     user_input = pd.DataFrame(
         {'itching': [itching], 'skin_rash': [skin_rash], 'nodal_skin_eruptions': [nodal_skin_eruptions],
          'continuous_sneezing': [continuous_sneezing],
          'shivering': [shivering], 'chills': [chills], 'joint_pain': [joint_pain], 'muscle_wasting': [muscle_wasting],
          'vomiting': [vomiting], 'fatigue': [fatigue], 'weight_loss': [weight_loss], 'restlessness': [restlessness],
-         'lethargy': [0], 'patches_in_throat': [0], 'irregular_sugar_level': [irregular_sugar_level], 'cough': [cough],
+         'lethargy': [lethargy], 'patches_in_throat': [patches_in_throat], 'irregular_sugar_level': [irregular_sugar_level], 'cough': [cough],
          'high_fever': [high_fever], 'breathlessness': [breathlessness], 'sweating': [sweating], 'headache': [headache],
-         'yellowish_skin': [0],  'dark_urine': [0], 'nausea': [0], 'loss_of_appetite': [0], 'pain_behind_the_eyes': [0], 'back_pain': [0],
-         'abdominal_pain': [0], 'diarrhoea': [0], 'mild_fever': [0], 'yellow_urine': [0], 'yellowing_of_eyes': [0],
-         'acute_liver_failure': [0], 'swelled_lymph_nodes': [0], 'malaise': [0], 'blurred_and_distorted_vision': [0],
-         'phlegm': [0], 'throat_irritation': [0], 'redness_of_eyes': [0], 'sinus_pressure': [0], 'runny_nose': [0],
-         'congestion': [0], 'chest_pain': [0], 'fast_heart_rate': [0], 'obesity': [0], 'excessive_hunger': [0],
-         'extra_marital_contacts': [0], 'loss_of_smell': [0], 'muscle_pain': [0], 'red_spots_over_body': [0],
-         'dischromic _patches': [0], 'watering_from_eyes': [0], 'increased_appetite': [0], 'polyuria': [0],
-         'family_history': [0], 'rusty_sputum': [0], 'receiving_blood_transfusion': [0],
-         'receiving_unsterile_injections': [0],
-         'coma': [0], 'stomach_bleeding': [0]})
+         'yellowish_skin': [yellowish_skin],  'dark_urine': [dark_urine], 'nausea': [nausea], 'loss_of_appetite': [loss_of_appetite],
+         'pain_behind_the_eyes': [pain_behind_the_eyes], 'back_pain': [back_pain],
+         'abdominal_pain': [abdominal_pain], 'diarrhoea': [diarrhoea], 'mild_fever': [mild_fever], 'yellow_urine': [yellow_urine], 'yellowing_of_eyes': [yellowing_of_eyes],
+         'acute_liver_failure': [acute_liver_failure], 'swelled_lymph_nodes': [swelled_lymph_nodes], 'malaise': [malaise], 'blurred_and_distorted_vision': [blurred_and_distorted_vision],
+         'phlegm': [phlegm], 'throat_irritation': [throat_irritation], 'redness_of_eyes': [redness_of_eyes], 'sinus_pressure': [sinus_pressure], 'runny_nose': [runny_nose],
+         'congestion': [congestion], 'chest_pain': [chest_pain], 'fast_heart_rate': [fast_heart_rate], 'obesity': [obesity], 'excessive_hunger': [excessive_hunger],
+         'extra_marital_contacts': [extra_marital_contacts], 'loss_of_smell': [loss_of_smell], 'muscle_pain': [muscle_pain], 'red_spots_over_body': [red_spots_over_body],
+         'dischromic_patches': [dischromic_patches], 'watering_from_eyes': [watering_from_eyes], 'increased_appetite': [increased_appetite], 'polyuria': [polyuria],
+         'family_history': [0], 'rusty_sputum': [rusty_sputum], 'receiving_blood_transfusion': [receiving_blood_transfusion],
+         'receiving_unsterile_injections': [receiving_unsterile_injections],
+         'coma': [0], 'stomach_bleeding': [stomach_bleeding]})
 
     print('helllp')
     prediction = load_model.predict(user_input)
--- app.py
+++ app.py
@@ -101,13 +101,64 @@ def symmp():
         cough = content['cough']
         high_fever = content['high_fever']
         breathlessness = content['breathlessness']
+        lethargy = content['lethargy']
+        patches_in_throat = content['patches_in_throat']
         sweating = content['sweating']
+        yellowish_skin = content['yellowish_skin']
+        dark_urine = content['dark_urine']
+        nausea = content['nausea']
+        loss_of_appetite = content['loss_of_appetite']
+        pain_behind_the_eyes = content['pain_behind_the_eyes']
+        back_pain = content['back_pain']
+        abdominal_pain = content['abdominal_pain']
+        diarrhoea = content['diarrhoea']
+        mild_fever = content['mild_fever']
+        yellow_urine = content['yellow_urine']
         headache = content['headache']
+        acute_liver_failure = content['acute_liver_failure']
+        swelled_lymph_nodes = content['swelled_lymph_nodes']
+        malaise = content['malaise']
+        blurred_and_distorted_vision = content['blurred_and_distorted_vision']
+        phlegm = content['phlegm']
+        throat_irritation = content['throat_irritation']
+        redness_of_eyes = content['redness_of_eyes']
+        sinus_pressure = content['sinus_pressure']
+        runny_nose = content['runny_nose']
+        congestion = content['congestion']
+        chest_pain = content['chest_pain']
+        fast_heart_rate = content['fast_heart_rate']
+        obesity = content['obesity']
+        excessive_hunger = content['excessive_hunger']
+        extra_marital_contacts = content['extra_marital_contacts']
+        loss_of_smell = content['loss_of_smell']
+        muscle_pain = content['muscle_pain']
+        red_spots_over_body = content['red_spots_over_body']
+        yellowing_of_eyes = content['yellowing_of_eyes']
+        increased_appetite = content['increased_appetite']
+        polyuria = content['polyuria']
+        watering_from_eyes = content['watering_from_eyes']
+        receiving_blood_transfusion = content['receiving_blood_transfusion']
+        receiving_unsterile_injections = content['receiving_unsterile_injections']
+        rusty_sputum = content['rusty_sputum']
+        stomach_bleeding = content['stomach_bleeding']
+        dischromic_patches = content['dischromic_patches']
+
         return SymptomPrediction(itching, skin_rash, nodal_skin_eruptions, continuous_sneezing, shivering, chills,
                                  joint_pain,
-                                 muscle_wasting, vomiting, fatigue, weight_loss, restlessness,
-                                 irregular_sugar_level, cough, high_fever,
-                                 breathlessness, sweating, headache)
+                                 muscle_wasting, vomiting, fatigue, weight_loss, restlessness, lethargy,
+                                 patches_in_throat, irregular_sugar_level, cough, high_fever,
+                                 breathlessness, sweating, headache, yellowish_skin, dark_urine, nausea,
+                                 loss_of_appetite, pain_behind_the_eyes,
+                                 back_pain, abdominal_pain, diarrhoea, mild_fever, yellow_urine, yellowing_of_eyes,
+                                 acute_liver_failure,
+                                 swelled_lymph_nodes, malaise, blurred_and_distorted_vision, phlegm, throat_irritation,
+                                 redness_of_eyes,
+                                 sinus_pressure, runny_nose, congestion, chest_pain, fast_heart_rate, obesity,
+                                 excessive_hunger, extra_marital_contacts,
+                                 loss_of_smell, muscle_pain, red_spots_over_body, dischromic_patches,
+                                 watering_from_eyes, increased_appetite, polyuria,
+                                 rusty_sputum, receiving_blood_transfusion, receiving_unsterile_injections,
+                                 stomach_bleeding)
         # except Exception as e:
         #     return e
     else:
